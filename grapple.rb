f49f8c7d014474fea78a6913257ffbb2e9ca0e5c
puyo <blue.puyo@gmail.com>
2009-05-22 13:53:10 +1000

    Updated Gosu/chipmunk version: polygons and pivot joints instead of circles or segments or slide joints.

--- grapple.rb
+++ grapple.rb
@@ -1,23 +1,7 @@
 require 'rubygems' rescue nil
 require 'gosu'
 require 'chipmunk'
-
-class Array
-  # e.g. [1,2,3].each_link yields [1,2], [2,3]
-  def each_link
-    prev = first
-    self[1, size].each do |item|
-      yield prev, item
-      prev = item
-    end
-  end
-
-  def each_edge
-    size.times do |n|
-      yield self[n], self[(n + 1) % size]
-    end
-  end
-end
+require 'extensions/array'
 
 def rot_point(body, vec2)
   x = vec2.x
@@ -57,7 +41,8 @@ def draw_circle(window, body, radius, colour, options=nil)
   end
 end
 
-# Convenience methods for converting between Gosu degrees, radians, and Vec2 vectors
+# Convenience methods for converting between Gosu degrees, radians, and Vec2
+# vectors
 class Numeric 
   def gosu_to_radians
     (self - 90) * Math::PI / 180.0
@@ -80,8 +65,6 @@ module Grapple
 
   INF = 1e100
 
-  GRAPPLE_SIZE = 0.3
-
   # The number of steps to process every Gosu update The Player ship can get
   # going so fast as to "move through" a star without triggering a collision;
   # an increased number of Chipmunk step calls per update will effectively
@@ -91,64 +74,77 @@ module Grapple
   class Rope
     include CP
 
-    attr_reader :links
-    attr_reader :circles
+    attr_reader :shapes
+    attr_reader :segment_length
+    def links() @bodies end
 
     def initialize(space, options=nil)
       options ||= {}
       @space = space
       @length = options[:length] || 100
-      @links = []
-      @circles = []
+      @bodies = []
+      @vertexes = []
+      @shapes = []
+      @segment_length = 10
       @length.times do |n|
-        link = Body.new(10, 10)
-        seg = Shape::Circle.new(link, 1.0, Vec2.new(0, 0))
-        #seg = Shape::Segment.new(link, Vec2.new(0, 0), Vec2.new(1, 0), 1)
-        seg.collision_type = :rope
-        seg.u = 0.5
-        seg.group = :grapple
-        space.add_body(link)
-        space.add_shape(seg)
-        @links << link
-        @circles << seg
+        vertexes = [
+          [-@segment_length/2, -2], # top left
+          [-@segment_length/2, +2], # bottom left
+          [+@segment_length/2, +2], # bottom right
+          [+@segment_length/2, -2], # top right
+        ].map{|x,y| Vec2.new(x, y) }
+        mass = 1
+        #body = Body.new(mass, moment_for_poly(mass, vertexes, Vec2.new(0, 0)))
+        body = Body.new(mass, 100)
+        body.p.x = n*@segment_length
+        shape = Shape::Poly.new(body, vertexes, Vec2.new(0, 0)) # body, verts, offset
+        shape.collision_type = :rope
+        shape.u = 0.1 # slippery
+        shape.group = :grapple
+        space.add_body(body)
+        space.add_shape(shape)
+        @bodies << body
+        @vertexes << vertexes
+        @shapes << shape
       end
 
-      @links.each_link do |prev, link|
-        joint = Joint::Slide.new(prev, link, Vec2.new(0.0, 0.0), Vec2.new(1.0, 0.0), 0.1, 10.0)
+      i = 0
+      @bodies.each_link do |prev_body, body|
+        joint = Joint::Pivot.new(prev_body, body, body.p - Vec2.new(@segment_length/2, 0))
         space.add_joint(joint)
+        i += 1
       end
     end
 
     def draw(window)
-      draw_gradient(window)
+      #draw_gradient(window)
+      draw_segments(window)
     end
 
     def draw_segments(window)
       col1 = Gosu::Color.new(0xffff0000)
       col2 = Gosu::Color.new(0xffffff00)
-      @links.each_link do |prev, link|
-        window.draw_line(prev.p.x, prev.p.y, col1, link.p.x, link.p.y, col2)
+      i = 0
+      @bodies.each_with_index do |body, i|
+        draw_poly(window, body, @vertexes[i], col2)
+        i += 1
       end
     end
 
     def draw_gradient(window)
       col = Gosu::Color.new(0xffff0000)
       i = 0
-      @links.each_link do |prev, link|
-        col.green = (255*i / @links.size)
+      @bodies.each_link do |prev, link|
+        col.green = (255*i / @bodies.size)
         window.draw_line(prev.p.x, prev.p.y, col, link.p.x, link.p.y, col)
         i += 1
-      end
-
-      @circles.each do |circle|
-        draw_circle(window, circle.body, 1.0, Gosu::Color.new(0xffffffff))
       end
     end
 
     def remove_first
-      if @links.any?
-        @space.remove_body(@links.shift)
-        @space.remove_shape(@circles.shift)
+      if @bodies.any?
+        @space.remove_body(@bodies.shift)
+        @space.remove_shape(@shapes.shift)
       end
     end
   end
@@ -159,18 +155,22 @@ module Grapple
     attr_reader :body
 
     def initialize(space)
-      @links = []
       @body = Body.new(INF, INF)
-      @p1, @p2 = Vec2.new(0, 500), Vec2.new(SCREEN_WIDTH, 440)
-      @width = 100
-      @seg = Shape::Segment.new(@body, @p1, @p2, @width)
-      @seg.collision_type = :ground
-      @seg.u = 0.99
-      space.add_static_shape(@seg)
+      @vertexes = [
+        [0, SCREEN_HEIGHT-200], # top left
+        [0, SCREEN_HEIGHT], # bottom left
+        [SCREEN_WIDTH, SCREEN_HEIGHT], # bottom right
+        [SCREEN_WIDTH, SCREEN_HEIGHT-200], # top right
+      ].map{|x,y| Vec2.new(x, y) }
+      @shape = Shape::Poly.new(@body, @vertexes, Vec2.new(0, 0)) # body, verts, offset
+      @shape.collision_type = :ground
+      @shape.group = :ground
+      @shape.u = 1.0
+      space.add_shape(@shape)
     end
 
     def draw(window)
-      draw_segment(window, body, [@p1, @p2], @width, Gosu::Color.new(0xff0000ff))
+      draw_poly(window, body, @vertexes, Gosu::Color.new(0xff0000ff))
     end
   end
 
@@ -181,7 +181,6 @@ module Grapple
 
     def initialize(space, options=nil)
       options ||= {}
-      @links = []
       @body = Body.new(INF, INF)
       @main_vertexes = [
         [-100, -100], # bottom left
@@ -189,10 +188,10 @@ module Grapple
         [100, 100], # top right
         [100, -100], # bottom right
       ].map{|x,y| Vec2.new(x, y) }
-      @main_shape = Shape::Poly.new(@body, @main_vertexes, Vec2.new(0,0)) # body, verts, offset
+      @main_shape = Shape::Poly.new(@body, @main_vertexes, Vec2.new(0, 0)) # body, verts, offset
       @main_shape.collision_type = :castle
       @main_shape.group = :castle
-      @main_shape.u = 0.99
+      @main_shape.u = 1.0
       space.add_shape(@main_shape)
 
       @snag_vertexes = [
@@ -201,14 +200,14 @@ module Grapple
         [-110, -110], # top right
         [-110, -100], # bottom right
       ].map{|x,y| Vec2.new(x, y) }
-      @snag_shape = Shape::Poly.new(@body, @snag_vertexes, Vec2.new(0,0)) # body, verts, offset
+      @snag_shape = Shape::Poly.new(@body, @snag_vertexes, Vec2.new(0, 0)) # body, verts, offset
       @snag_shape.collision_type = :castle
       @snag_shape.group = :castle
-      @snag_shape.u = 0.99
+      @snag_shape.u = 1.0
       space.add_shape(@snag_shape)
 
       #@radius = 200
-      #@circle = Shape::Circle.new(@body, @radius, Vec2.new(0.0, 0.0))
+      #@circle = Shape::Circle.new(@body, @radius, Vec2.new(0, 0))
       #@circle.collision_type = :castle
       #@circle.u = 0.5
       #@circle.group = :castle
@@ -226,37 +225,61 @@ module Grapple
     include CP
 
     attr_reader :body
-    attr_reader :shape
+    attr_reader :shaft_v
+    attr_reader :cross_v
+    attr_reader :left_tip_v
+    attr_reader :right_tip_v
 
-    def initialize(space, options=nil)
-      options ||= {}
-      @links = []
-      @body = Body.new(options[:mass] || 100, options[:moment] || 100)
-      @vertexes = [
-        [-5, -50],
-        [-5, 25],
-        [-22, 25],
-        [-37, 15],
-        [-27, 35],
-        [-5, 35],
-        [0, 50],
-        [5, 35],
-        [27, 35],
-        [37, 15],
-        [25, 25],
-        [5, 25],
-        [5, -50],
-      ].map{|x,y| Vec2.new(GRAPPLE_SIZE*x, GRAPPLE_SIZE*y) }
-      @shape = Shape::Poly.new(@body, @vertexes, Vec2.new(0,0)) # body, verts, offset
-      @shape.collision_type = :hook
-      @shape.u = 0.99
-      @shape.group = :grapple
+    def initialize(space)
+      @shaft_v = [
+        [-50,   -5],  # top left
+        [-50,    5],  # bottom left
+        [ 35.5,  5],  # bottom right
+        [ 51,    0],  # tip (far right)
+        [ 35.5, -5],  # top right
+      ]
+      @cross_v = [
+        [24, -24],  # top left
+        [35, -24],  # top right
+        [35,  24],  # bottom right
+        [24,  24],  # bottom left
+      ]
+      @left_tip_v = [
+        [24, -24],
+        [35, -24],
+        [15, -37],
+        [24, -24],
+      ]
+      @right_tip_v = [
+        [24, 24],
+        [35, 24],
+        [15, 37],
+        [24, 24],
+      ]
+      @vertexes = [@shaft_v, @cross_v, @left_tip_v, @right_tip_v]
+
+      scale = 0.3
+      for v in @vertexes
+        v.map!{|x,y| Vec2.new(scale*(x - 14), scale*y) }
+      end
+
+      mass = 30
+      moment = @vertexes.map{|v| moment_for_poly(mass, v, Vec2.new(0, 0)) }.inject(0){|s,m| s += m }
+      @body = Body.new(mass, moment)
       space.add_body(@body)
-      space.add_shape(@shape)
+      for v in @vertexes
+        shape = Shape::Poly.new(@body, v, Vec2.new(0, 0)) # body, verts, offset
+        shape.collision_type = :hook
+        shape.u = 1.0
+        shape.group = :grapple
+        space.add_shape(shape)
+      end
     end
 
     def draw(window)
-      draw_poly(window, @body, @vertexes, Gosu::Color.new(0xffff0000))
+      for v in @vertexes
+        draw_poly(window, @body, v, Gosu::Color.new(0xffff0000))
+      end
     end
   end
 
@@ -264,12 +287,12 @@ module Grapple
     include CP
 
     def initialize
-      super(SCREEN_WIDTH, SCREEN_HEIGHT, true, 16)
+      super(SCREEN_WIDTH, SCREEN_HEIGHT, false, 16)
       self.caption = "Grapple"
 
       @space = Space.new
       @space.damping = 0.8    
-      @space.gravity = Vec2.new(0.0, 10.0)
+      @space.gravity = Vec2.new(0, 10.0)
       @dt = (1.0/60.0)
 
       @ground = Ground.new(@space)
@@ -278,16 +301,22 @@ module Grapple
 
       @rope = Rope.new(@space, :length => 45)
       @hook = GrappleHook.new(@space)
-      joint = Joint::Pin.new(@hook.body, @rope.links.last, Vec2.new(0.0, -20.0 * GRAPPLE_SIZE), Vec2.new(0.0, 0.0))
+      # attach rope and hook
+      between = @rope.links.last.p + Vec2.new(@rope.segment_length/2, 0)
+      @hook.body.p = between + Vec2.new(-@hook.shaft_v.first.x, 0)
+      joint = Joint::Pivot.new(@hook.body, @rope.links.last, between)
       @space.add_joint(joint)
       reset_hook
 
-      attach = Joint::Slide.new(@ground.body, @rope.links.first, @grapple_origin, Vec2.new(0.0, 0.0), 0, 0)
+      # attach other end of rope to ground
+      @rope.links.first.p = @grapple_origin
+      attach = Joint::Pivot.new(@ground.body, @rope.links.first, @grapple_origin)
       @space.add_joint(attach)
 
+      # make a nice hanging rope
       @rope2 = Rope.new(@space, :length => 10)
       hanging = Body.new(INF, INF)
-      joint = Joint::Pin.new(hanging, @rope2.links.first, Vec2.new(0.0, 0.0), Vec2.new(0.0, 0.0))
+      joint = Joint::Pin.new(hanging, @rope2.links.first, Vec2.new(0, 0), Vec2.new(0, 0))
       @space.add_joint(joint)
       @rope2.links.each_with_index{|link, i| link.p = Vec2.new(100 - 10*i, 200) }
       hanging.p = Vec2.new(150, 0)
@@ -296,37 +325,29 @@ module Grapple
       @castle.body.p = Vec2.new(500, 250)
 
       @space.add_collision_func(:hook, :hook) do
-        p 'hook hook ' + rand.to_s
+        #p 'hook hook ' + rand.to_s
       end
       @space.add_collision_func(:rope, :rope) do
-        p 'rope rope ' + rand.to_s
+        #p 'rope rope ' + rand.to_s
       end
       @space.add_collision_func(:hook, :rope) do
-        p 'hook rope ' + rand.to_s
+        #p 'hook rope ' + rand.to_s
       end
       @space.add_collision_func(:rope, :hook) do
-        p 'rope hook ' + rand.to_s
+        #p 'rope hook ' + rand.to_s
       end
       @space.add_collision_func(:hook, :ground) do
-        p 'hook ground ' + rand.to_s
+        #p 'hook ground ' + rand.to_s
       end
       @space.add_collision_func(:hook, :castle) do
-        p 'hook castle ' + rand.to_s
+        #p 'hook castle ' + rand.to_s
       end
       @space.add_collision_func(:rope, :castle) do
-        p 'rope castle ' + rand.to_s
+        #p 'rope castle ' + rand.to_s
       end
     end
 
     def update
-      # Check keyboard
-      if button_down? Gosu::Button::KbSpace
-        @hook.body.v = Vec2.new(50, -100)
-      end
-      if button_down? char_to_button_id('r')
-        reset_hook
-      end
-
       SUBSTEPS.times do
         @space.step(@dt)
       end
@@ -347,11 +368,23 @@ module Grapple
 
     def button_down(id)
       case id
+      when Gosu::Button::KbUp
+        @hook.body.v += Vec2.new(0, -100)
+      when Gosu::Button::KbDown
+        @hook.body.v += Vec2.new(0, 100)
+      when Gosu::Button::KbLeft
+        @hook.body.v += Vec2.new(-100, 0)
+      when Gosu::Button::KbRight
+        @hook.body.v += Vec2.new(100, 0)
+      when Gosu::Button::KbSpace # 'throw'
+        @hook.body.v += Vec2.new(100, -200)
+      when char_to_button_id('r')
+        reset_hook
       when Gosu::Button::KbEscape, char_to_button_id('q')
         close
       when char_to_button_id('p')
         @rope.remove_first
-        attach = Joint::Slide.new(@ground.body, @rope.links.first, @grapple_origin, Vec2.new(0.0, 0.0), 0, 0)
+        attach = Joint::Slide.new(@ground.body, @rope.links.first, @grapple_origin, Vec2.new(0, 0), 0, 0)
         @space.add_joint(attach)
       end
     end
